346f0b782cb28da8df080690b2f47837b44f5e8c
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-10-04 17:54:40 +0200

    Q shaping: initial

--- common.mk
+++ common.mk
@@ -1,20 +1,20 @@
 # Overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/themes/overlays/common/
+#PRODUCT_PACKAGE_OVERLAYS += vendor/themes/overlays/common/
 
 # Accents
-include vendor/themes/accents/accents.mk
+#include vendor/themes/accents/accents.mk
 
 # Boot animation
 include vendor/themes/bootanimation/bootanimation.mk
 
 # System Icon Switcher
-include vendor/themes/sysiconswitcher/sysiconswitcher.mk
+#include vendor/themes/sysiconswitcher/sysiconswitcher.mk
 
 # Themes
-include vendor/themes/uithemes/uithemes.mk
+#include vendor/themes/uithemes/uithemes.mk
 
 # ThumbUI
 include vendor/themes/ThumbUI/thumbui.mk
 
 # UI Switcher
-include vendor/themes/uiswitcher/uiswitcher.mk
+#include vendor/themes/uiswitcher/uiswitcher.mk
