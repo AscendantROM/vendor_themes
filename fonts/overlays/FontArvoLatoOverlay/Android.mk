f5b3126ba9625605f34fa4f3d3df5ef2318d620d
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-01-21 06:39:35 +0100

    f

--- fonts/overlays/FontArvoLatoOverlay/Android.mk
+++ fonts/overlays/FontArvoLatoOverlay/Android.mk
@@ -17,15 +17,13 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_RRO_THEME := FontArvoLato
-LOCAL_CERTIFICATE := platform
-LOCAL_PRODUCT_MODULE := true
-
-LOCAL_SRC_FILES := $(call all-subdir-java-files)
-
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_PACKAGE_NAME := FontArvoLatoOverlay
 LOCAL_SDK_VERSION := current
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := false
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
-include $(BUILD_RRO_PACKAGE)
+include $(BUILD_PACKAGE)
+
