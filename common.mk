fa07c6ed7566366bde9df0990d5c3df172056334
Dil3mm4 <dil3mm4.dev@gmail.com>
2020-02-02 19:23:04 +0100

    Common: crap

--- common.mk
+++ common.mk
@@ -1,5 +1,6 @@
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += vendor/themes/overlays/common/
+PRODUCT_PACKAGE_OVERLAYS +=)vendor/themes/overlay/common/
 
 # Accents
 include vendor/themes/accents/accents.mk
